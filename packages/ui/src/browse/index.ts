12693ad9d6704a55b4f237efd50a725cdfc25866
Chris Hasson <hassoncs@gmail.com>
2026-02-19 11:20:43 -0800

    feat: consolidate browse components into @slopcade/ui

---
+++ packages/ui/src/browse/index.ts
@@ -0,0 +1,7 @@
+export { FilterBar } from "./FilterBar";
+export { GameCard, GameGridCard, Pagination } from "./GameCard";
+export { GameDetailPanel } from "./GameDetailPanel";
+export { GameMetaBadge } from "./GameMetaBadge";
+export { TutorialPager } from "./TutorialPager";
+export { TutorialStep } from "./TutorialStep";
+export type { HowToPlayStep, PartyTemplate } from "./types";
