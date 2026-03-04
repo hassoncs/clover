e6521aa7aaf1b1fee012ef000d9913f31eac27ca
Chris Hasson <hassoncs@gmail.com>
2026-03-03 20:46:01 -0800

    feat(landing): integrate effects library into landing page

---
+++ apps/landing-slopcade/src/components/effects/index.ts
@@ -0,0 +1,10 @@
+// Effects library barrel file
+export { HeroShaderScene } from "./HeroShaderScene";
+export { ScrollDirector } from "./ScrollDirector";
+export { ScrollReveal } from "./ScrollReveal";
+export type {
+	AnimationType,
+	SplitBy,
+	SplitHeadlineProps,
+} from "./SplitHeadline";
+export { SplitHeadline } from "./SplitHeadline";
